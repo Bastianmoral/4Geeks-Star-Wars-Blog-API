39c1a416a0808469679a0b8560a5d49c0f0ee0c0
Bastianmoral <BASTIHC@gmail.com>
2022-03-30 03:07:20 +0000

    Swapi for 4 geeks

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Character, Planet, Vehicle, Favoritescharacter, Favoritesplanet, Favoritesvehicle
 #from models import Person
 
 app = Flask(__name__)
@@ -32,12 +32,136 @@ def sitemap():
 
 @app.route('/user', methods=['GET'])
 def handle_hello():
-
+    query_user = User.query.all()
+    query_user = list(map(lambda User: username.serialize(), query_user))
     response_body = {
-        "msg": "Hello, this is your GET /user response "
+        "msg": "Hello, this is your GET /user response ",
+        "users":query_user
     }
 
     return jsonify(response_body), 200
+
+@app.route('/character', methods=['GET'])
+def get_character():
+    query_character = Character.query.all()
+    query_character = list(map(lambda Character: Character.serialize(), query_character))
+    response_body = {
+        "msg": "Hello, this is your GET /Character response",
+        "Character" : query_character
+    }
+
+    return jsonify(response_body), 200
+
+@app.route('/character/<int:character_id>', methods=['DELETE'])
+def delete_character(id):
+    character_delete = Character.query.get(id)
+    if not character_delete:
+        response_body = {
+            "msg": "Hello, this is your DELETE /Character response",
+            "Character" : "Character doesn't exist, can't be eliminated"
+        }
+        return jsonify(response_body), 200
+    db.session.delete(character_delete)
+    db.session.commit()
+    response_body = {
+        "msg": "Hello, this is your DELETE /Character response",
+        "Character" : "Character eliminated"
+    }
+
+@app.route('/character', methods=['POST'])
+def post_character():
+    body = request.get_json()
+    character = Character(character_name=body['character_name'],  home_planet=body['home_planet'])
+    db.session.add(character)
+    db.session.commit()
+    response_body = {
+        "msg": "Hello, this is your POST /character response "
+    }
+
+    return jsonify(response_body), 200
+
+@app.route('/planet', methods=['GET'])
+def get_planets():
+    query_planet = Planet.query.all()
+    query_planet = list(map(lambda Planet: Planet.serialize(), query_planet))
+    response_body = {
+        "msg": "Hello, this is your GET /planet response",
+        "Planet" : query_planet
+    }
+
+    return jsonify(response_body), 200
+
+@app.route('/planet/<int:planet_id>', methods=['DELETE'])
+def delete_planet(id):
+    planet_delete = Planet.query.get(id)
+    if not planet_delete:
+        response_body = {
+            "msg": "Hello, this is your DELETE /planet response",
+            "Planet" : "Planet doesn't exist, can't be eliminated"
+        }
+        return jsonify(response_body), 200
+    db.session.delete(planet_delete)
+    db.session.commit()
+    response_body = {
+        "msg": "Hello, this is your DELETE /planet response",
+        "Planet" : "Planet eliminated"
+    }
+
+
+    return jsonify(response_body), 200
+
+@app.route('/planet', methods=['POST'])
+def post_planet():
+    body = request.get_json()
+    planet = Planet(planet_name=body['planet_name'], density=body['density'], population=body['population'])
+    db.session.add(planet)
+    db.session.commit()
+    response_body = {
+        "msg": "Hello, this is your POST /planet response "
+    }
+
+    return jsonify(response_body), 200
+
+@app.route('/vehicle', methods=['GET'])
+def get_vehicles():
+    query_vehicles = Vehicle.query.all()
+    query_vehicles = list(map(lambda Vehicle: Vehicle.serialize(), query_vehicles))
+    response_body = {
+        "msg": "Hello, this is your GET /vehicles response",
+        "Vehicle" : query_vehicles
+    }
+
+    return jsonify(response_body), 200
+
+@app.route('/vehicle/<int:vehicle_id>', methods=['DELETE'])
+def delete_vehicle(id):
+    vehicle_delete = Vehicle.query.get(id)
+    if not vehicle_delete:
+        response_body = {
+            "msg": "Hello, this is your DELETE /vehicle response",
+            "Vehicle" : "Vehicle doesn't exist, can't be eliminated"
+        }
+        return jsonify(response_body), 200
+    db.session.delete(vehicle_delete)
+    db.session.commit()
+    response_body = {
+        "msg": "Hello, this is your DELETE /vehicle response",
+        "Vehicle" : "Vehicle eliminated"
+    }
+
+    return jsonify(response_body), 200
+
+@app.route('/vehicles', methods=['POST'])
+def post_vehicles():
+    body = request.get_json()
+    vehicle = Vehicles(vehicle_name=body['vehicle_name'],vehicle_model=body['vehicle_model'],vehicle_manufacturer=body['vehicle_manufacturer'],vehicle_pilot=body['vehicle_pilot'])
+    db.session.add(Vehicle)
+    db.session.commit()
+    response_body = {
+        "msg": "Hello, this is your POST /vehicles response "
+    }
+
+    return jsonify(response_body), 200  
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -2,6 +2,143 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
+class Character(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    character_name = db.Column(db.String(150), nullable=False)
+    home_planet = db.Column(db.String(120), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<Character %r>' % self.character_name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "character_name": self.character_name,
+            "home_planet": self.home_planet,
+        }
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    planet_name = db.Column(db.String(150), nullable=False)
+    population = db.Column(db.Integer)
+    density = db.Column(db.String(50), nullable=False) 
+    
+    def __repr__(self):
+        return '<Planet %r>' % self.planet_name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "planet_name": self.planet_name,
+            "population": self.population,
+            "density": self.density,
+        }
+
+class Vehicle(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    vehicle_name = db.Column(db.String(150), nullable=False)
+    vehicle_model = db.Column(db.String(150),nullable=False)
+    vehicle_manufacturer = db.Column(db.String(150),nullable=False)
+    vehicle_pilot = db.Column(db.String(150))
+    
+    def __repr__(self):
+        return '<Vehicle %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.vehicle_name,
+            "model": self.vehicle_model,
+            "manufacturer": self.vehicle_manufacturer,
+            "pilots": self.vehicle_pilots
+        }
+
+class Favoritescharacter(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    character_id = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "character_id": self.character_id,
+            "user_id":self.user_id
+        }
+
+class Favoritesplanet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    planet_id = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "planet_id": self.planet_id,
+            "user_id":self.user_id
+        }
+
+class Favoritesvehicle(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    vehicle_id = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "vehicle_id": self.vehicle_id,
+            "user_id":self.user_id
+        }
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
+    username = db.Column(db.String(100), nullable=False, unique=True)
+    first_name = db.Column(db.String(100), nullable=False)
+    last_name = db.Column(db.String(100), nullable=False)
+    
+    def __repr__(self):
+        return '<User %r>' % self.first_name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "email": self.email,
+            "Favoritescharacter": self.getFavoritescharacter(),
+            "Favoritesplanet": self.getFavoritesplanet(),
+            "Favoritesvehicle": self.getFavoritesvehicle(),
+        }
+
+    def getFavoritescharacter(self):
+        return list(map(lambda fav: fav.serialize(),self.favorite_character))
+    
+    def getFavoritesPlanet(self):
+        return list(map(lambda fav: fav.serialize(),self.favorite_character))
+    
+    def getFavoritescharacter(self):
+        return list(map(lambda fav: fav.serialize(),self.favorite_character))
+    
+
+
+
+
+""" from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -16,4 +153,4 @@ class User(db.Model):
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
-        }
+        } """
